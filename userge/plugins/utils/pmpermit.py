d7af6f493b064898240492db19fb9ce6f624fb47
sachink4s <67131301+sachink4s@users.noreply.github.com>
2020-08-13 16:24:00 +0530

    Update pmpermit.py

--- userge/plugins/utils/pmpermit.py
+++ userge/plugins/utils/pmpermit.py
@@ -214,7 +214,7 @@ async def uninvitedPmHandler(message: Message):
     else:
         pmCounter.update({message.from_user.id: 1})
         await message.reply(
-            noPmMessage.format_map(SafeDict(**user_dict)) + '\n`- This Chat is secured by SK4S`')
+            noPmMessage.format_map(SafeDict(**user_dict)) + '\n`This Chat is secured by SK4S`')
         await asyncio.sleep(1)
         await CHANNEL.log(f"#NEW_MESSAGE\n{user_dict['mention']} has messaged you")
 
